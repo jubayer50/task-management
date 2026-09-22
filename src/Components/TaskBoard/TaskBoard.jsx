4ce942641f96b219c54e8d1f6b152bcf22dbff46
jubayer50 <aujubayer1@gmail.com>
2026-09-22 23:55:54 +0600

    save a new task function implemented

--- src/Components/TaskBoard/TaskBoard.jsx
+++ src/Components/TaskBoard/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const defaultTask = {
   id: crypto.randomUUID(),
@@ -15,9 +16,20 @@ const defaultTask = {
 
 const TaskBoard = () => {
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+
+    setShowModal(false);
+  };
+
+  console.log("taskboard taks", tasks);
 
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal onSave={handleAddTask}></AddTaskModal>}
+
       <div className="container mx-auto px-6 md:px-8">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
@@ -25,7 +37,7 @@ const TaskBoard = () => {
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction></TaskAction>
+          <TaskAction onAddingTask={() => setShowModal(true)}></TaskAction>
 
           <TaskList tasks={tasks}></TaskList>
         </div>
